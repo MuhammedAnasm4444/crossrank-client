b371fec3a14788b6d83a1f4478396028e0750437
Muhammed Anas <muhammedanasm4444@gmail.com>
2021-06-17 02:09:15 +0530

    fixed some

--- src/Components/User/Dashboard/Dashboard.js
+++ src/Components/User/Dashboard/Dashboard.js
@@ -23,10 +23,10 @@ function Dashboard() {
     try {
       console.log("entering fjdkjafk jkjk jaxiosggg")
     
-      axios.get("/admin/challenges").then((res) => {
+      axios.get("http://34.122.147.138:8080/admin/challenges").then((res) => {
         console.log(res.data)
         console.log("dd")
-        setChallenges([])
+        setChallenges(res.data)
       }).catch((err)=> {
         console.log(err)
         console.log("error not working")
